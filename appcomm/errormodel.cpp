e40a9ebda49a45c09800d30aa06bddfa805dc027
Manuela <manuela.mondini.2003@gmail.com>
2026-03-23 16:23:50 +0100

    feat(#41): implemented errormodel.cpp

--- appcomm/errormodel.cpp
+++ appcomm/errormodel.cpp
@@ -2,12 +2,35 @@
 
 namespace errormodel {
 
-bool AppCommError::hasError(){
-    return true;
+// ==================== AppCommError ====================
+
+bool AppCommError::hasError() const
+{
+    return code != ErrorCode::None;
 }
 
-QString AppCommError::toString(){
-    return "";
+QString errorCodeToString(ErrorCode code)
+{
+    switch (code) {
+    case ErrorCode::None: return "None";
+    case ErrorCode::AuthError: return "AuthError";
+    case ErrorCode::NetworkError: return "NetworkError";
+    case ErrorCode::ParseError: return "ParseError";
+    case ErrorCode::ProtocolError: return "ProtocolError";
+    case ErrorCode::PermissionError: return "PermissionError";
+    case ErrorCode::TimeoutError: return "TimeoutError";
+    case ErrorCode::RateLimitExceeded: return "RateLimitExceeded";
+    case ErrorCode::Unknown: return "Unknown";
+    }
+    return "Unknown";
 }
 
+QString AppCommError::toString() const
+{
+    return QString("Error[%1] (%2): %3")
+    .arg(errorCodeToString(code))
+        .arg(httpStatus)
+        .arg(message);
 }
+
+} // namespace errormodel
