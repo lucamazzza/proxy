10de49c3684a0a16794d47127335268adff68f0c
Luca Mazza <luca.mazza@mazluc.ch>
2026-03-25 20:01:36 +0100

    test(#25): Test realtime + garbage collector

---
+++ test/tst_garbagecollector.cpp
@@ -0,0 +1,144 @@
+/*!
+ * @file tst_garbagecollector.cpp
+ * @brief Unit tests for the GarbageCollector service
+ *
+ * Tests TTL-based message cleanup and batch deletion.
+ *
+ * @copyright Copyright (c) 2026 SUPSI
+ */
+
+#include <QtTest>
+#include <QSignalSpy>
+#include "../appcomm/garbagecollector.h"
+#include "../appcomm/appwritesdk.h"
+#include "../appcomm/model.h"
+
+using namespace appcomm::server;
+using namespace appcomm::model;
+using namespace appwritesdk;
+
+class TestGarbageCollector : public QObject {
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void init();
+    void cleanup();
+
+    // Constructor tests
+    void testConstructor();
+
+    // Cleanup tests
+    void testRunCleanupWithZeroTTL();
+    void testRunCleanupWithNegativeTTL();
+    void testRunCleanupWithValidTTL();
+
+    // Signal tests
+    void testCleanupCompleteSignal();
+    void testCleanupErrorSignal();
+
+private:
+    Server *m_server;
+    ConnectionConfig m_config;
+};
+
+void TestGarbageCollector::initTestCase() {
+    // Setup test configuration
+    m_config.endpoint = "https://cloud.appwrite.io/v1";
+    m_config.projectId = "test-project-123";
+    m_config.apiKey = "test-api-key";
+    m_config.dbId = "test-db";
+    m_config.collectionId = "messages";
+    
+    m_server = new Server(new QNetworkAccessManager(this), this);
+}
+
+void TestGarbageCollector::cleanupTestCase() {
+    // Server will be deleted by Qt parent-child relationship
+}
+
+void TestGarbageCollector::init() {
+    // Reset before each test
+}
+
+void TestGarbageCollector::cleanup() {
+    // Clean up after each test
+}
+
+void TestGarbageCollector::testConstructor() {
+    GarbageCollector gc(m_server, m_config);
+    
+    QVERIFY(true); // Constructor should not crash
+}
+
+void TestGarbageCollector::testRunCleanupWithZeroTTL() {
+    GarbageCollector gc(m_server, m_config);
+    
+    QSignalSpy spyComplete(&gc, &GarbageCollector::cleanupComplete);
+    QSignalSpy spyError(&gc, &GarbageCollector::cleanupError);
+    
+    PersistencePolicy policy;
+    policy.messageTTL = 0; // Should skip cleanup
+    
+    gc.runCleanup(policy);
+    
+    // Should emit cleanupComplete(0) immediately
+    QCOMPARE(spyComplete.count(), 1);
+    QCOMPARE(spyError.count(), 0);
+    
+    QList<QVariant> arguments = spyComplete.takeFirst();
+    QCOMPARE(arguments.at(0).toInt(), 0);
+}
+
+void TestGarbageCollector::testRunCleanupWithNegativeTTL() {
+    GarbageCollector gc(m_server, m_config);
+    
+    QSignalSpy spyComplete(&gc, &GarbageCollector::cleanupComplete);
+    
+    PersistencePolicy policy;
+    policy.messageTTL = -1; // Should skip cleanup
+    
+    gc.runCleanup(policy);
+    
+    // Should emit cleanupComplete(0) immediately
+    QCOMPARE(spyComplete.count(), 1);
+    
+    QList<QVariant> arguments = spyComplete.takeFirst();
+    QCOMPARE(arguments.at(0).toInt(), 0);
+}
+
+void TestGarbageCollector::testRunCleanupWithValidTTL() {
+    GarbageCollector gc(m_server, m_config);
+    
+    QSignalSpy spyComplete(&gc, &GarbageCollector::cleanupComplete);
+    QSignalSpy spyError(&gc, &GarbageCollector::cleanupError);
+    
+    PersistencePolicy policy;
+    policy.messageTTL = 30; // 30 days
+    
+    gc.runCleanup(policy);
+    
+    // In real scenario, this would make a network request
+    // For unit tests without network, we just verify no crash
+    QVERIFY(true);
+}
+
+void TestGarbageCollector::testCleanupCompleteSignal() {
+    GarbageCollector gc(m_server, m_config);
+    
+    QSignalSpy spy(&gc, &GarbageCollector::cleanupComplete);
+    
+    QVERIFY(spy.isValid());
+}
+
+void TestGarbageCollector::testCleanupErrorSignal() {
+    GarbageCollector gc(m_server, m_config);
+    
+    QSignalSpy spy(&gc, &GarbageCollector::cleanupError);
+    
+    QVERIFY(spy.isValid());
+}
+
+QTEST_MAIN(TestGarbageCollector)
+#include "tst_garbagecollector.moc"
---
+++ test/tst_realtime.cpp
@@ -0,0 +1,140 @@
+/*!
+ * @file tst_realtime.cpp
+ * @brief Unit tests for the Realtime WebSocket client
+ *
+ * Tests WebSocket connection, event parsing, and error handling.
+ *
+ * @copyright Copyright (c) 2026 SUPSI
+ */
+
+#include <QtTest>
+#include <QSignalSpy>
+#include <QJsonDocument>
+#include "../appcomm/realtime.h"
+#include "../appcomm/appwritesdk.h"
+
+using namespace appcomm;
+using namespace appwritesdk;
+
+class TestRealtime : public QObject {
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void init();
+    void cleanup();
+
+    // Constructor tests
+    void testConstructor();
+    void testConstructorWithConfig();
+
+    // Connection tests
+    void testConnectBuildsCorrectUrl();
+    void testDisconnect();
+
+    // Signal tests
+    void testConnectedSignal();
+    void testDisconnectedSignal();
+    void testEventReceivedSignal();
+    void testErrorOccurredSignal();
+
+private:
+    ConnectionConfig m_config;
+};
+
+void TestRealtime::initTestCase() {
+    // Setup test configuration
+    m_config.endpoint = "https://cloud.appwrite.io/v1";
+    m_config.projectId = "test-project-123";
+    m_config.apiKey = "test-api-key";
+    m_config.dbId = "test-db";
+    m_config.collectionId = "test-collection";
+}
+
+void TestRealtime::cleanupTestCase() {
+    // Nothing to clean up
+}
+
+void TestRealtime::init() {
+    // Reset before each test
+}
+
+void TestRealtime::cleanup() {
+    // Clean up after each test
+}
+
+void TestRealtime::testConstructor() {
+    Realtime realtime(m_config);
+    
+    QVERIFY(true); // Constructor should not crash
+}
+
+void TestRealtime::testConstructorWithConfig() {
+    ConnectionConfig config;
+    config.endpoint = "https://example.com/v1";
+    config.projectId = "project123";
+    
+    Realtime realtime(config);
+    
+    QVERIFY(true); // Should accept custom config
+}
+
+void TestRealtime::testConnectBuildsCorrectUrl() {
+    Realtime realtime(m_config);
+    
+    QStringList channels;
+    channels << "databases.test-db.collections.messages.documents";
+    
+    // Note: This will attempt to connect to real server, which will fail in tests
+    // In a real scenario, you'd mock the QWebSocket
+    // For now, just verify the method doesn't crash
+    realtime.connect(channels);
+    
+    QVERIFY(true);
+}
+
+void TestRealtime::testDisconnect() {
+    Realtime realtime(m_config);
+    
+    realtime.disconnect();
+    
+    QVERIFY(true); // Should not crash when disconnecting unconnected socket
+}
+
+void TestRealtime::testConnectedSignal() {
+    Realtime realtime(m_config);
+    
+    QSignalSpy spy(&realtime, &Realtime::connected);
+    
+    QVERIFY(spy.isValid());
+    QCOMPARE(spy.count(), 0); // No connection in tests
+}
+
+void TestRealtime::testDisconnectedSignal() {
+    Realtime realtime(m_config);
+    
+    QSignalSpy spy(&realtime, &Realtime::disconnected);
+    
+    QVERIFY(spy.isValid());
+}
+
+void TestRealtime::testEventReceivedSignal() {
+    Realtime realtime(m_config);
+    
+    QSignalSpy spy(&realtime, &Realtime::eventReceived);
+    
+    QVERIFY(spy.isValid());
+    QCOMPARE(spy.count(), 0);
+}
+
+void TestRealtime::testErrorOccurredSignal() {
+    Realtime realtime(m_config);
+    
+    QSignalSpy spy(&realtime, &Realtime::errorOccurred);
+    
+    QVERIFY(spy.isValid());
+}
+
+QTEST_MAIN(TestRealtime)
+#include "tst_realtime.moc"
