2efda73d8a5eee0f49e403563c7779aa0ca85406
Manuela <manuela.mondini@supsi.ch>
2026-04-01 13:22:46 +0200

    feat(#25): added basic cpp file for appcommclient

--- appcomm/appcommclient.cpp
+++ appcomm/appcommclient.cpp
@@ -5,3 +5,8 @@ using namespace appcomm::client;
 AppcommClient::AppcommClient(QObject *parent)
     : QObject{parent}
 {}
+
+ConnectionState AppcommClient::connectionState() const
+{
+    return m_connectionState;
+}
