e2b90ff05395904987a4e54bcc1d875dcc91c948
Manuela <manuela.mondini.2003@gmail.com>
2026-04-27 20:26:07 +0200

    added: configuration to demo

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,17 +1,27 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QQmlEngine>
 
 #include "democontroller.h"
+#include "../appcomm/model.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    DemoController controller;
+    appcomm::model::AppCommConfig config;
+    config.endpoint = "https://fra.cloud.appwrite.io/v1";
+    config.projectId = "69a007d7000a5a976062";
+    config.databaseId = "default";
+    config.messagesCollectionId = "messages";
+    config.incomingMessagesCollectionId = "pendingmessages";
+    config.membersCollectionId = "members";
 
-    engine.rootContext()->setContextProperty("controller", &controller);
+    QQmlApplicationEngine engine;
+    DemoController controller(config);
+
+    qmlRegisterSingletonInstance("App", 1, 0, "AppController", &controller);
 
     QObject::connect(
         &engine,
